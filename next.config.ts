f3fc8e25b0486931b3e558aa2f6824576953518d
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-02-17 06:59:31 +0530

    Add google and unsplash remote pattern fo images in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
