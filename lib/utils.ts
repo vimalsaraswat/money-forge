dd85a49313de5679dbc7b0e6dbed64e1cee58eaf
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-02-18 07:21:53 +0530

    Add generateRandomColor and capitalize utility fns

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateRandomColor() {
+  const getRandomColorComponent = () => {
+    return Math.floor(Math.random() * 150) + 50;
+  };
+
+  const r = getRandomColorComponent();
+  const g = getRandomColorComponent();
+  const b = getRandomColorComponent();
+
+  return `rgb(${r},${g},${b})`;
+}
+
+export function capitalize(str: string) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
