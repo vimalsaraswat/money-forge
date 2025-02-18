e4db604053dece1e79b896d37afa2ac39b280655
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-02-18 08:33:58 +0530

    Add loading page

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoadingPage = () => (
+  <div className="z-50 w-full h-full flex justify-center items-center bg-gray-50 opacity-75">
+    <div className="animate-spin rounded-full size-10 border-t-3 border-b-3 border-gray-600"></div>
+  </div>
+);
+
+export default LoadingPage;
