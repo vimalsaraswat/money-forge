5f9b9992598c77ecabd5bcf3864235e48b70b79b
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-02-19 23:48:17 +0530

    Add dark theme

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const LoadingPage = () => (
-  <div className="z-50 w-full h-full flex justify-center items-center bg-gray-50 opacity-75">
-    <div className="animate-spin rounded-full size-10 border-t-3 border-b-3 border-gray-600"></div>
+  <div className="z-50 w-full h-full flex justify-center items-center opacity-75">
+    <div className="animate-spin rounded-full size-10 border-t-3 border-b-3 border-primary"></div>
   </div>
 );
 
