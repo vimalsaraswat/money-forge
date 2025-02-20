79348cad9b8a939474783db8c21b6ffabc01acc4
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-02-20 22:47:19 +0530

    Add transaction cards for mobile

--- components/transaction-list.tsx
+++ components/transaction-list.tsx
@@ -16,50 +16,100 @@ export default function TransactionList({
   transactions: TransactionType[];
 }) {
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Type</TableHead>
-          <TableHead>Amount</TableHead>
-          <TableHead>Category</TableHead>
-          <TableHead>Date</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
+    <>
+      <div className="md:hidden">
         {transactions?.length > 0 ? (
           transactions?.map((transaction) => (
-            <TableRow key={transaction.id}>
-              <TableCell
-                className={cn(
-                  "capitalize",
-                  transaction?.type === TransactionEnum.INCOME
-                    ? "text-green-500"
-                    : "text-destructive",
-                )}
-              >
-                {transaction.type}
-              </TableCell>
-              <TableCell>{formatCurrency(transaction?.amount)}</TableCell>
-              <TableCell className="capitalize">
-                {transaction.category}
-              </TableCell>
-              <TableCell>
+            <div
+              key={transaction.id}
+              className="border rounded-md p-4 mb-4 shadow-sm"
+            >
+              <div className="flex justify-between">
+                <div className="capitalize text-sm text-muted-foreground">
+                  {transaction.category}
+                </div>
+                <div>{formatCurrency(transaction?.amount)}</div>
+              </div>
+              <div className="text-sm text-muted-foreground">
                 {new Date(transaction.date).toLocaleDateString()}
-              </TableCell>
-              <TableCell className="space-x-2">
-                <TransactionForm transaction={transaction} editMode={true} />
-                <DeleteTransaction transactionId={transaction?.id} />
-              </TableCell>
-            </TableRow>
+              </div>
+              <div className="flex items-center justify-between">
+                <div
+                  className={cn(
+                    "capitalize font-semibold",
+                    transaction?.type === TransactionEnum.INCOME
+                      ? "text-green-500"
+                      : "text-destructive",
+                  )}
+                >
+                  {transaction.type}
+                </div>
+                <div className="mt-2 flex items-center justify-end space-x-2">
+                  <TransactionForm transaction={transaction} editMode={true} />
+                  <DeleteTransaction transactionId={transaction?.id} />
+                </div>
+              </div>
+            </div>
           ))
         ) : (
-          <TableRow>
-            <td colSpan={4} className="text-center text-muted-foreground py-20">
-              No transactions found
-            </td>
-          </TableRow>
+          <div className="text-center text-muted-foreground py-10">
+            No transactions found
+          </div>
         )}
-      </TableBody>
-    </Table>
+      </div>
+      <div className="hidden md:block">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Type</TableHead>
+              <TableHead>Amount</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead>Date</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {transactions?.length > 0 ? (
+              transactions?.map((transaction) => (
+                <TableRow key={transaction.id}>
+                  <TableCell
+                    className={cn(
+                      "capitalize",
+                      transaction?.type === TransactionEnum.INCOME
+                        ? "text-green-500"
+                        : "text-destructive",
+                    )}
+                  >
+                    {transaction.type}
+                  </TableCell>
+                  <TableCell>{formatCurrency(transaction?.amount)}</TableCell>
+                  <TableCell className="capitalize">
+                    {transaction.category}
+                  </TableCell>
+                  <TableCell>
+                    {new Date(transaction.date).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell className="space-x-2">
+                    <TransactionForm
+                      transaction={transaction}
+                      editMode={true}
+                    />
+                    <DeleteTransaction transactionId={transaction?.id} />
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <td
+                  colSpan={4}
+                  className="text-center text-muted-foreground py-20"
+                >
+                  No transactions found
+                </td>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </>
   );
 }
