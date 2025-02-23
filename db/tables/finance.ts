025aa21e5846120ba27880e28f51ca8c1f9e9f76
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-02-23 22:19:48 +0530

    Create budget and category tables

--- db/tables/finance.ts
+++ db/tables/finance.ts
@@ -1,29 +1,73 @@
-import { text, timestamp, pgEnum, doublePrecision } from "drizzle-orm/pg-core";
+import {
+  text,
+  timestamp,
+  pgEnum,
+  doublePrecision,
+  boolean,
+} from "drizzle-orm/pg-core";
 import { users } from "./auth";
+import { TransactionEnum } from "@/types";
 import { createTable } from "../table-creator";
-import { CategoryEnum, TransactionEnum } from "@/types";
 
 export const transactionTypeEnum = pgEnum(
   "transactionType",
   Object.values(TransactionEnum) as [string, ...string[]],
 );
-export const categoryEnum = pgEnum(
-  "category",
-  Object.values(CategoryEnum) as [string, ...string[]],
-);
 
-export const transactions = createTable("transaction", {
+export const categories = createTable("categories", {
   id: text("id")
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
   userId: text("userId")
     .notNull()
-    .references(() => users.id, { onDelete: "cascade" }),
-  amount: doublePrecision("amount").notNull(),
-  category: categoryEnum("category").notNull(),
+    .references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  name: text("name").notNull(),
+  description: text("description"),
   type: transactionTypeEnum("type").notNull(),
+  isPublic: boolean("isPublic").default(false).notNull(),
+  createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow(),
+  deletedAt: timestamp("deletedAt", { mode: "date" }),
+});
+
+export const transactions = createTable("transactions", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  amount: doublePrecision("amount").notNull(),
+  categoryId: text("categoryId")
+    .notNull()
+    .references(() => categories.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
   date: timestamp("date", { mode: "date" }).notNull(),
   description: text("description"),
   createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
   updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow(),
+  deletedAt: timestamp("deletedAt", { mode: "date" }),
+});
+
+export const budgets = createTable("budgets", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  categoryId: text("categoryId")
+    .notNull()
+    .references(() => categories.id, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    }),
+  amount: doublePrecision("amount").notNull(),
+  startDate: timestamp("startDate", { mode: "date" }).notNull(),
+  endDate: timestamp("endDate", { mode: "date" }).notNull(),
+  createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).defaultNow(),
+  deletedAt: timestamp("deletedAt", { mode: "date" }),
 });
