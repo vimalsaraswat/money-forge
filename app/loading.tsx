f1ef6ff5a608984bda5c9b545d471353faef92d6
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-02-27 22:53:22 +0530

    Add spinner component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,6 @@
 import React from "react";
+import Spinner from "@/components/spinner";
 
-const LoadingPage = () => (
-  <div className="z-50 w-full h-full flex justify-center items-center opacity-75">
-    <div className="animate-spin rounded-full size-10 border-t-3 border-b-3 border-primary"></div>
-  </div>
-);
+const LoadingPage = () => <Spinner />;
 
 export default LoadingPage;
---
+++ components/spinner.tsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="z-50 w-full h-full flex justify-center items-center opacity-75">
+    <div className="animate-spin rounded-full size-10 border-t-3 border-b-3 border-primary"></div>
+  </div>
+);
+
+export default Spinner;
