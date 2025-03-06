d5a7fc9bf981598fea43a73ef08653c351643449
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-03-06 08:01:08 +0530

    Fix transactionId validation

--- actions/transaction.ts
+++ actions/transaction.ts
@@ -21,8 +21,8 @@ const TransactionSchema = z.object({
   type: z.nativeEnum(TransactionEnum, {
     errorMap: () => ({ message: "Please select a transaction type." }),
   }),
-  categoryId: z.string({
-    errorMap: () => ({ message: "Please select a category." }),
+  categoryId: z.string().uuid({
+    message: "Please select a category.",
   }),
   date: z.string().refine((val) => !isNaN(new Date(val).getTime()), {
     message: "Invalid date format.",
