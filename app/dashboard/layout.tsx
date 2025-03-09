406bcb5590ccdc8c743523f98643dc1d2a239648
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-03-09 17:37:34 +0530

    Add video for unauthenticated user on dashboad

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,33 @@
+import { auth } from "@/auth";
+import Image from "next/image";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  const allowedEmails = process.env.ALLOWED_EMAIL?.split(",");
+
+  if (
+    (allowedEmails?.length ?? -1) > 0 &&
+    session?.user &&
+    allowedEmails?.some((email) => email === session?.user?.email)
+  )
+    return <>{children}</>;
+
+  return (
+    <div className="h-full w- full flex items-center justify-center">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="h-full object-cover aspect-[9/16]"
+      >
+        <source src="/404.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
