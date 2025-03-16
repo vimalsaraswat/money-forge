a59eb6e378a3ad61ddf459fdc0516903334978f5
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-03-16 15:12:05 +0530

    Add coming soon to chat

--- components/ai-chat.tsx
+++ components/ai-chat.tsx
@@ -59,83 +59,86 @@ export default function Chat() {
   };
 
   return (
-    <div className="flex flex-col justify-center flex-1 max-w-3xl mx-auto">
-      <div className="flex flex-col justify-between gap-2 flex-1 overflow-auto">
-        <div
-          ref={messagesContainerRef}
-          className="flex-1 flex flex-col gap-3 w-full items-center overflow-y-auto pt-16"
-        >
-          {conversation.length === 0 && (
-            <motion.div
-              className="h-[350px] px-4 w-full max-w-md md:px-0"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.2 }}
-            >
-              <motion.div
-                className="text-lg rounded-lg p-6 flex flex-col gap-4 text-card-foreground bg-card/50"
-                whileHover={{ scale: 1.03 }}
-                transition={{ duration: 0.3 }}
-              >
-                <motion.p
-                  className="flex flex-row justify-center gap-4 items-center text-zinc-900 dark:text-zinc-50"
-                  initial={{ opacity: 0, scale: 0.8 }}
-                  animate={{ opacity: 1, scale: 1 }}
-                  transition={{ duration: 0.4, delay: 0.4 }}
-                >
-                  {session?.user?.image ? (
-                    <Image
-                      width={40}
-                      height={40}
-                      src={session?.user?.image}
-                      alt={"User avatar"}
-                      className="rounded-full"
-                    />
-                  ) : (
-                    <SparklesIcon size={40} className="animate-bounce" />
-                  )}
-                </motion.p>
-                <motion.p
-                  className="text-center"
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 0.5, delay: 0.6 }}
-                >
-                  Hi {session?.user?.name || "there"},<br /> I&apos;m your
-                  personal finance buddy!
-                  <br /> I can answer questions about budgeting, saving,
-                  investing, and more.
-                </motion.p>
-              </motion.div>
-            </motion.div>
-          )}
-          {conversation.map((message, i) => (
-            <React.Fragment key={i}>{message}</React.Fragment>
-          ))}
-          <div ref={messagesEndRef} />
-        </div>
-
-        <form action={handleSubmit} className="flex relative items-center">
-          <Input
-            ref={inputRef}
-            placeholder="Ask me about your finance..."
-            value={input}
-            onChange={(event) => {
-              setInput(event.target.value);
-            }}
-            className="pr-10"
-            autoFocus
-          />
-          <SubmitButton
-            size="icon"
-            variant="outline"
-            loading={<Loader className="animate-spin" />}
-            className="absolute right-0 bottom-0 opacity-80 cursor-pointer"
+    <>
+      <div>ComingSoon</div>
+      <div className="flex-col justify-center flex-1 max-w-3xl mx-auto hidden">
+        <div className="flex flex-col justify-between gap-2 flex-1 overflow-auto">
+          <div
+            ref={messagesContainerRef}
+            className="flex-1 flex flex-col gap-3 w-full items-center overflow-y-auto pt-16"
           >
-            <SendIcon />
-          </SubmitButton>
-        </form>
+            {conversation.length === 0 && (
+              <motion.div
+                className="h-[350px] px-4 w-full max-w-md md:px-0"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+              >
+                <motion.div
+                  className="text-lg rounded-lg p-6 flex flex-col gap-4 text-card-foreground bg-card/50"
+                  whileHover={{ scale: 1.03 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <motion.p
+                    className="flex flex-row justify-center gap-4 items-center text-zinc-900 dark:text-zinc-50"
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.4, delay: 0.4 }}
+                  >
+                    {session?.user?.image ? (
+                      <Image
+                        width={40}
+                        height={40}
+                        src={session?.user?.image}
+                        alt={"User avatar"}
+                        className="rounded-full"
+                      />
+                    ) : (
+                      <SparklesIcon size={40} className="animate-bounce" />
+                    )}
+                  </motion.p>
+                  <motion.p
+                    className="text-center"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5, delay: 0.6 }}
+                  >
+                    Hi {session?.user?.name || "there"},<br /> I&apos;m your
+                    personal finance buddy!
+                    <br /> I can answer questions about budgeting, saving,
+                    investing, and more.
+                  </motion.p>
+                </motion.div>
+              </motion.div>
+            )}
+            {conversation.map((message, i) => (
+              <React.Fragment key={i}>{message}</React.Fragment>
+            ))}
+            <div ref={messagesEndRef} />
+          </div>
+
+          <form action={handleSubmit} className="flex relative items-center">
+            <Input
+              ref={inputRef}
+              placeholder="Ask me about your finance..."
+              value={input}
+              onChange={(event) => {
+                setInput(event.target.value);
+              }}
+              className="pr-10"
+              autoFocus
+            />
+            <SubmitButton
+              size="icon"
+              variant="outline"
+              loading={<Loader className="animate-spin" />}
+              className="absolute right-0 bottom-0 opacity-80 cursor-pointer"
+            >
+              <SendIcon />
+            </SubmitButton>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
