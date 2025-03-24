a02597dc179d0ea356f383129ea5494f8d6f4cc5
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-03-24 09:39:12 +0530

    Add budget stacked chart component

---
+++ components/dashboard/budget-stackedchart.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { BudgetListType } from "@/types";
+import { Bar, BarChart, ResponsiveContainer, XAxis } from "recharts";
+
+const chartConfig = {
+  expense: {
+    label: "Expense",
+    color: "hsl(var(--chart-1))",
+  },
+  budget: {
+    label: "Budget",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
+
+export default function BudgetStackedChart({
+  data,
+  className,
+}: {
+  data: BudgetListType;
+  className?: string;
+}) {
+  return (
+    <Card className={className}>
+      <CardHeader>
+        <CardTitle>Budget vs Expenditure</CardTitle>
+        <CardDescription>
+          Overview of budget compared to expenditure.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 grid place-items-center">
+        <BudgetChart data={data} />
+      </CardContent>
+    </Card>
+  );
+}
+
+function BudgetChart({ data }: { data: BudgetListType }) {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <ChartContainer config={chartConfig} className="min-h-[300px] w-full">
+        <BarChart accessibilityLayer data={data}>
+          <XAxis
+            dataKey="category"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+          />
+          <Bar
+            dataKey="amount"
+            stackId="a"
+            fill="var(--color-expense)"
+            radius={[0, 0, 4, 4]}
+          />
+          <Bar
+            dataKey="spent"
+            stackId="a"
+            fill="var(--color-budget)"
+            radius={[4, 4, 0, 0]}
+          />
+          <ChartTooltip
+            content={<ChartTooltipContent indicator="line" />}
+            cursor={false}
+            defaultIndex={1}
+          />
+        </BarChart>
+      </ChartContainer>
+    </ResponsiveContainer>
+  );
+}
